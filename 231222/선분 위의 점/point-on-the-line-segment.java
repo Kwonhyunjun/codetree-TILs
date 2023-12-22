f87b08a19e5d372177293836e94eb8f38b8b01ec
권현준 <119548924+Kwonhyunjun@users.noreply.github.com>
2023-12-22 18:56:22 +0900

    Title: 선분 위의 점, Time: 287ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 231222/선분 위의 점/point-on-the-line-segment.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M, hole[]; 
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken()); 
+
+        st = new StringTokenizer(br.readLine());
+        hole = new int[N];
+        for(int i=0; i<N; i++){
+            hole[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i=0; i<M; i++){
+            st = new StringTokenizer(br.readLine());
+
+            int n1 = Integer.parseInt(st.nextToken());
+            int n2 = Integer.parseInt(st.nextToken());
+
+            
+            int cnt = upperBound(n2) - lowerBound(n1);  
+            System.out.println(cnt);           
+        }
+    }
+
+    static int upperBound(int target){
+        int left = 0; 
+        int right = N-1; 
+        int minIdx = N; 
+
+        while(left <= right){
+            int mid = left + (right - left)/2;
+
+            if(hole[mid] > target){
+                right = mid - 1; 
+                minIdx = Math.min(minIdx, mid); 
+            }else{
+                left = mid + 1; 
+            }
+        }
+        return minIdx; 
+    }
+
+    static int lowerBound(int target){
+        int left = 0; 
+        int right = N-1; 
+        int minIdx = N; 
+
+        while(left <= right){
+            int mid = left + (right - left)/2;
+
+            if(hole[mid] >= target){
+                right = mid - 1; 
+                minIdx = Math.min(minIdx, mid); 
+            }else{
+                left = mid + 1; 
+            }
+        }
+        return minIdx; 
+    }
+}
