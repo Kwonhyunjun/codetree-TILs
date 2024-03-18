f889d664c83d5a33c9018a95223535b8c135871e
권현준 <119548924+Kwonhyunjun@users.noreply.github.com>
2024-03-18 19:25:16 +0900

    Title: 모이자, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240318/모이자/gather.java
@@ -0,0 +1,32 @@
+import java.io.*; 
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st; 
+        StringBuilder sb = new StringBuilder(); 
+
+        int N = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+
+        for(int i = 0 ; i < 5 ; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int ans = Integer.MAX_VALUE;
+
+        for(int i=0; i<N; i++){
+            int temp = 0;
+            for(int j=0; j<N; j++){
+                if(i == j) continue;
+                temp += Math.abs(i-j) * arr[j];
+            }
+            ans = Math.min(ans, temp); 
+        }
+
+        System.out.println(ans);
+
+    }
+}
