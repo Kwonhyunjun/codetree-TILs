1e4e582d443a9198d56b20101bfce6672ffa3898
권현준 <119548924+Kwonhyunjun@users.noreply.github.com>
2024-07-11 21:55:19 +0900

    Title: G or H 3, Time: 86ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240711/G or H 3/G-or-H-3.java
+++ 240711/G or H 3/G-or-H-3.java
@@ -10,26 +10,35 @@ public class Main {
         int K = Integer.parseInt(st.nextToken());
 
         int[] arr = new int[10000];
-        int max_idx = 0; 
+        int min_idx = 10001;
+        int max_idx = 0;
         int answer = 0;
+        int total = 0; 
 
         for(int i=0; i<N; i++){
             st = new StringTokenizer(br.readLine());
             int idx = Integer.parseInt(st.nextToken())-1;
             int score = ("G".equals(st.nextToken())) ? 1 : 2;
+            min_idx = Math.min(idx, min_idx);
             max_idx = Math.max(idx, max_idx); 
             arr[idx] = score;
+            total += score; 
         }
 
-        for(int i=0; i<=max_idx - K; i++){
-            // System.out.println("i : "+i); 
+        if((max_idx - min_idx) <= K){
+            System.out.println(total);
+            return; 
+        }
+
+        for(int i=min_idx; i<=max_idx - K; i++){
+            System.out.println("i : "+i); 
             int temp = 0;
             for(int j=0; j<=K; j++){
                 // System.out.printf("arr[%d] = %d ", i+j, arr[i+j]); 
                 temp += arr[i+j]; 
             }
             // System.out.println();
-            // System.out.println(temp); 
+            System.out.println(temp); 
             answer = Math.max(answer, temp); 
         }
 
