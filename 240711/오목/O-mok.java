b3a3abded89b4ae690e622d034203cc3f694a051
권현준 <119548924+Kwonhyunjun@users.noreply.github.com>
2024-07-11 10:35:12 +0900

    Title: 오목, Time: 96ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240711/오목/O-mok.java
+++ 240711/오목/O-mok.java
@@ -4,13 +4,16 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        char[][] map = new char[19][19];
+        StringTokenizer st; 
+        int[][] map = new int[19][19];
 
         for(int i=0; i<19; i++){
-            map[i] = br.readLine().toCharArray();
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<19; j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
         }
-
+        
         int[] dr = {-1, -1, -1, 0, 1, 1, 1, 0};
         int[] dc = {-1, 0, 1, 1, 1, 0, -1, -1};
 
@@ -21,17 +24,18 @@ public class Main {
 
         for(int r=0; r<19; r++){
             for(int c=0; c<19; c++){
-                System.out.printf("시작 map[%d][%d]\n", r, c);
-                if(map[r][c] == '0') continue;
+                // System.out.printf("map[%d][%d]\n", r, c);
+                if(map[r][c] == 0) continue;
                 if(find){
-                    System.out.printf("%d \n %d %d", winner-48, midR, midC);
+                    System.out.printf("%d\n%d %d", winner, midR, midC);
                     return;
                 }
 
-                char cur = map[r][c];
-                System.out.printf("시작 map[%d][%d] = %c\n", r, c, cur);
+                int cur = map[r][c];
+                // System.out.printf("시작 map[%d][%d] = %d\n", r, c, map[r][c]);
 
                 for(int d =0; d<8; d++){
+                    // System.out.printf("방향 : %d\n", d);
                     int nr = r + dr[d];
                     int nc = c + dc[d];
                     
@@ -40,20 +44,19 @@ public class Main {
                         cnt++;
                         
                         if(cnt >= 5) {
-                            winner = cur-48; 
-                            find = true; 
+                            // System.out.printf("오목 map[%d][%d] = %d 방향(%d), cur:%d\n", r, c, map[r][c], d, cur);
+                            winner = cur; 
+                            find = true;
+                            int reverseR = (dr[d] == 0) ? 0 : (dr[d] == 1)? -1 : 1;
+                            int reverseC = (dc[d] == 0) ? 0 : (dc[d] == 1)? -1 : 1; 
+                            // System.out.printf("nr(%d) nc(%d) dr(%d) reverseR(%d) dc(%d) reverseC(%d)\n", nr, nc, dr[d], reverseR, dc[d], reverseC);
+                            midR = nr + reverseR * 2 + 1;
+                            midC = nc + reverseC * 2 + 1; 
                             break;
                         }
 
-                        nr = r + dr[d];
-                        nc = c + dc[d];
-                    }
-
-                    if(find){
-                        int reverseR = (dr[d] == 0) ? 0 : (dr[d] == 1)? -1 : 1;
-                        int reverseC = (dc[d] == 0) ? 0 : (dc[d] == 1)? -1 : 1; 
-                        midR = nr - reverseR * 2;
-                        midC = nc - reverseC * 2; 
+                        nr = nr + dr[d];
+                        nc = nc + dc[d];
                     }
                 }
 
