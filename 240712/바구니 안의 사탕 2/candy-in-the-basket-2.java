4001748f296f1747924448326118054caedf7b69
권현준 <119548924+Kwonhyunjun@users.noreply.github.com>
2024-07-12 13:42:16 +0900

    Title: 바구니 안의 사탕 2, Time: 95ms, Memory: 8MB, Status: Passed - CodeTree

--- 240712/바구니 안의 사탕 2/candy-in-the-basket-2.java
+++ 240712/바구니 안의 사탕 2/candy-in-the-basket-2.java
@@ -12,17 +12,24 @@ public class Main {
         int[] arr = new int[101]; 
 
         int answer = Integer.MIN_VALUE;
-
+        int total = 0;
         for(int i=0; i<N; i++){
             st = new StringTokenizer(br.readLine());
             int candy = Integer.parseInt(st.nextToken());
             int idx = Integer.parseInt(st.nextToken());
 
             arr[idx] += candy; 
+            total += candy;
+        }
+
+        if(K > 100){
+            System.out.println(total);
+            return; 
         }
 
         for(int i = 0; i<=100-K ; i++){
             int temp = 0;
+            
             for(int j= i-K; j <= i+K; j++){
                 if(j < 0 || j > 100) continue;
                 temp += arr[j]; 
