1cd53e988c065b009b52ac33d2f8cecc8845b0a9
권현준 <119548924+Kwonhyunjun@users.noreply.github.com>
2024-07-27 16:18:19 +0900

    Title: 겹치지 않게 선분 고르기, Time: 133ms, Memory: 9MB, Status: Passed - CodeTree

--- 240727/겹치지 않게 선분 고르기/select-segments-without-overlap.java
+++ 240727/겹치지 않게 선분 고르기/select-segments-without-overlap.java
@@ -29,37 +29,47 @@ public class Main {
     static void solve(int cnt){
         if(cnt >= N){
             answer = Math.max(list.size(), answer); 
+            // for(int[] line : list){
+            //     System.out.print(Arrays.toString(line) + " "); 
+            // }
+            // System.out.println(); 
             return; 
         }
 
+        // System.out.println("cnt :" + cnt);
+        // for(int[] line : list){
+        //     System.out.print(Arrays.toString(line) + " "); 
+        // }
+        // System.out.println();
+
         // 추가
         if(!isOver(cnt)){
+            // System.out.println(cnt +" : "+ Arrays.toString(lines[cnt]) + " 추가");
             list.add(new int[]{lines[cnt][0], lines[cnt][1]});
             solve(cnt+1); 
             list.remove(list.size()-1); 
         }
 
         // 추가 안함
+        // System.out.println(cnt + " 생략");
         solve(cnt+1);
     }
 
     static boolean isOver(int cnt){
-        int start = lines[cnt][0];
-        int end = lines[cnt][0];
+        int start1 = lines[cnt][0];
+        int end1 = lines[cnt][1];
         
         for(int i=0; i<list.size(); i++){
-            if(!check(start, end, list.get(i)[0], list.get(i)[1]))
-                return true; 
-        }
+            int start2 = list.get(i)[0];
+            int end2 = list.get(i)[1];
 
+            // System.out.printf("[%d, %d], [%d, %d]\n", start1, end1, start2, end2); 
+            if(!((end1 < start2) || (end2 < start1))){
+                // System.out.println("겹친다");
+                return true;
+            } 
+        }
+        // System.out.println("겹치지 않는다"); 
         return false; 
-    }
-
-    static boolean check(int x1, int x2, int y1, int y2){
-        if((x2 < y1) || (y2 < x1)){ // 겹치지 않으면
-            return true;
-        }
-
-        return false;
     }
 }
