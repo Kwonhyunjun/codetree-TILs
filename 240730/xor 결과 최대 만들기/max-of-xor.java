faebdaf7ad1b9618e647496b0ca4dfcf1aff31d6
권현준 <119548924+Kwonhyunjun@users.noreply.github.com>
2024-07-30 09:49:23 +0900

    Title: xor 결과 최대 만들기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240730/xor 결과 최대 만들기/max-of-xor.java
+++ 240730/xor 결과 최대 만들기/max-of-xor.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Main {
 
-    static int N, M, answer;
+    static int N, M, answer, arr[];
     static ArrayList<Integer> list = new ArrayList<>(); 
 
     public static void main(String[] args) throws Exception{
@@ -13,18 +13,26 @@ public class Main {
         N = Integer.parseInt(st.nextToken()); 
         M = Integer.parseInt(st.nextToken()); 
 
-        comb(0, 0); 
+        arr = new int[5]; 
+
+        st = new StringTokenizer(br.readLine()); 
+        for(int i=0; i<N; i++){
+            arr[i] = Integer.parseInt(st.nextToken()); 
+        }
+
+        comb(0, -1); 
 
         System.out.println(answer); 
     }
 
     static void comb(int idx, int start){
         if(idx == M){
+            // System.out.println(list); 
             answer = Math.max(answer, calc()); 
             return; 
         }
 
-        for(int i=start+1; i<=N; i++){
+        for(int i=start+1; i<N; i++){
             list.add(i);
             comb(idx+1, i);
             list.remove(list.size()-1); 
@@ -32,10 +40,10 @@ public class Main {
     }
     
     static int calc(){
-        int res = list.get(0); 
+        int res = arr[list.get(0)]; 
 
         for(int i=1; i<M; i++){
-            res ^= list.get(i);
+            res ^= arr[list.get(i)];
         }
         
         return res; 
