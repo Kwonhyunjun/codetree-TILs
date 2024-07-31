7e59b2646ddbbdade6426d6cf31e90bd65c67436
권현준 <119548924+Kwonhyunjun@users.noreply.github.com>
2024-07-31 10:51:35 +0900

    Title: 그래프 탐색, Time: 211ms, Memory: 17MB, Status: Passed - CodeTree

--- 240731/그래프 탐색/graph-traversal.java
+++ 240731/그래프 탐색/graph-traversal.java
@@ -16,7 +16,7 @@ public class Main {
 
         // 방문배열 (그래프 탐색 알고리즘 대원칙 2번 - 이미 방문한 정점은 다시는 방문하지 않는다.)
         v = new boolean[N];
-        
+
         // 인접행렬
         adjMatrix = new int[N][N];
         
@@ -40,7 +40,7 @@ public class Main {
         }
 
         v[0] = true;
-        dfs1(0); 
+        dfs2(0); 
 
         System.out.println(answer);
     }
@@ -57,6 +57,22 @@ public class Main {
                 answer++;  
                 dfs1(i); 
             }
+        }
+    }
+
+    /*
+        인접리스트를 잉요한 DFS
+    */
+
+    public static void dfs2(int vertex){
+        ArrayList<Integer> cur = adjList[vertex];
+        
+        for(int i=0; i<cur.size(); i++){
+            if(v[cur.get(i)]) continue; 
+            
+            v[cur.get(i)] = true;
+            answer++; 
+            dfs2(cur.get(i)); 
         }
     }
 }
