9cc9ea8c8174e5d9427b113cabb50cc4a9a450bf
권현준 <119548924+Kwonhyunjun@users.noreply.github.com>
2024-08-02 14:11:50 +0900

    Title: 피보나치 수, Time: 97ms, Memory: 8MB, Status: Passed - CodeTree

--- 240802/피보나치 수/fibonacci-number.java
+++ 240802/피보나치 수/fibonacci-number.java
@@ -1,52 +1,52 @@
 import java.io.*;
 import java.util.*; 
 
-// public class Main {
-
-//     static int N; 
-
-//     public static void main(String[] args) throws Exception{
-//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-//         N = Integer.parseInt(br.readLine());
-
-//         memo = new int[N+1]; 
-//         Arrays.fill(memo, -1); 
-        
-//         System.out.println(fibbo(N));   
-//     }
-
-//     static int[] memo;
-//     public static int fibbo(int n){
-//         if(n <= 2){
-//             return 1; 
-//         }
-
-//         if(memo[n] != -1) return memo[n]; 
-
-//         int res = fibbo(n-2) + fibbo(n-1); 
-//         memo[n] = res; 
-//         return res; 
-//     }
-// }
-
 public class Main {
-    
-    static int N, dp[]; 
+
+    static int N; 
 
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         N = Integer.parseInt(br.readLine());
-        dp = new int[N+10];
 
-        dp[1] = 1;
-        dp[2] = 1;
+        memo = new int[N+1]; 
+        Arrays.fill(memo, -1); 
+        
+        System.out.println(fibbo(N));   
+    }
 
-        for(int i=3; i<=N; i++){
-            dp[i] = dp[i-2] + dp[i-1]; 
+    static int[] memo;
+    public static int fibbo(int n){
+        if(n <= 2){
+            return 1; 
         }
 
-        System.out.println(dp[N]);
+        if(memo[n] != -1) return memo[n]; 
+
+        int res = fibbo(n-2) + fibbo(n-1); 
+        memo[n] = res; 
+        return res; 
     }
 }
+
+// public class Main {
+    
+//     static int N, dp[]; 
+
+//     public static void main(String[] args) throws Exception{
+//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+//         N = Integer.parseInt(br.readLine());
+//         dp = new int[N+10];
+
+//         dp[1] = 1;
+//         dp[2] = 1;
+
+//         for(int i=3; i<=N; i++){
+//             dp[i] = dp[i-2] + dp[i-1]; 
+//         }
+
+//         System.out.println(dp[N]);
+//     }
+// }
