ac07836655092c26e4e9d8d18181842d5afbfd87
권현준 <119548924+Kwonhyunjun@users.noreply.github.com>
2024-08-02 14:26:50 +0900

    Title: 계단 오르기, Time: 104ms, Memory: 8MB, Status: Passed - CodeTree

--- 240802/계단 오르기/climbing-stairs.java
+++ 240802/계단 오르기/climbing-stairs.java
@@ -3,18 +3,31 @@ import java.util.*;
 
 public class Main {
     
-    static int N, memo[]; 
+    static int N, memo[], dp[]; 
 
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
         N = Integer.parseInt(br.readLine()); 
-        memo = new int[N+1]; 
+        // TOP-DOWN
+        // memo = new int[N+1]; 
 
-        Arrays.fill(memo, -1); 
+        // Arrays.fill(memo, -1); 
 
-        System.out.println(fibbo(N));
+        // System.out.println(fibbo(N));
 
+        // BOTTOM-UP
+        dp = new int[1001];
+
+        dp[0] = 1; 
+        dp[1] = 0;
+        dp[2] = 1; 
+
+        for(int i=3; i<=N; i++){
+            dp[i] = (dp[i-2] + dp[i-3]) % 10007; 
+        }
+
+        System.out.println(dp[N]); 
     }
 
     public static int fibbo(int n){
