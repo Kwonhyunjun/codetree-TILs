2b235d12d5c4d7716ca46fda029125d34087687e
권현준 <119548924+Kwonhyunjun@users.noreply.github.com>
2024-08-04 16:37:21 +0900

    Title: 서로 다른 BST 개수 세기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240804/서로 다른 BST 개수 세기/number-of-unique-bst.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*; 
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[N+1]; 
+        
+        dp[0] = 1; 
+        dp[1] = 1; 
+        dp[2] = 2; 
+
+        for(int i=3; i<=N; i++){
+            int res = 0;
+            for(int j=1; j<=i; j++){
+                // System.out.printf("%d %d\n", j-1, i-j); 
+                res += dp[j-1] * dp[i-j]; 
+            }
+            dp[i] = res; 
+        }
+
+        System.out.println(dp[N]); 
+    }
+}
