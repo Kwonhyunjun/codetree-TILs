5b6161a04bf715904b12db8f0abd91d6523cbe20
권현준 <119548924+Kwonhyunjun@users.noreply.github.com>
2024-08-04 16:38:15 +0900

    Title: 서로 다른 BST 개수 세기, Time: 99ms, Memory: 8MB, Status: Passed - CodeTree

--- 240804/서로 다른 BST 개수 세기/number-of-unique-bst.java
+++ 240804/서로 다른 BST 개수 세기/number-of-unique-bst.java
@@ -7,7 +7,7 @@ public class Main {
         
         int N = Integer.parseInt(br.readLine());
 
-        int[] dp = new int[N+1]; 
+        int[] dp = new int[20]; 
         
         dp[0] = 1; 
         dp[1] = 1; 
