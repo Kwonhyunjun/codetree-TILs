4848a25d9afe27f387edeefc599bfc9754e42c7a
권현준 <119548924+Kwonhyunjun@users.noreply.github.com>
2024-08-04 17:26:09 +0900

    Title: 정수 사각형 최대 합, Time: 132ms, Memory: 12MB, Status: Passed - CodeTree

--- 240804/정수 사각형 최대 합/maximum-sum-path-in-square.java
+++ 240804/정수 사각형 최대 합/maximum-sum-path-in-square.java
@@ -8,7 +8,7 @@ public class Main {
 
         int N = Integer.parseInt(st.nextToken()); 
         int[][] map = new int[N][N]; 
-        int[][] dp = new int[N][N];
+        long[][] dp = new long[N][N];
 
         for(int i=0; i<N; i++){
             st = new StringTokenizer(br.readLine()); 
@@ -22,7 +22,13 @@ public class Main {
 
         for(int i=1; i<N; i++){
             dp[i][0] = dp[i-1][0] + map[i][0]; 
+            dp[0][i] = dp[0][i-1] + map[0][i];  
         }
+
+
+        // for(int i=0; i<N; i++){
+        //     System.out.println(Arrays.toString(dp[i])); 
+        // }
         
         for(int r=1; r<N; r++){
             for(int c=1; c<N; c++){
