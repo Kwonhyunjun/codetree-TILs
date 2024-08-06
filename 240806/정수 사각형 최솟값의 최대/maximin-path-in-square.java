c8693da4a936f7456e75ab4820e5741686c414da
권현준 <119548924+Kwonhyunjun@users.noreply.github.com>
2024-08-06 10:02:37 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 175ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240806/정수 사각형 최솟값의 최대/maximin-path-in-square.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st; 
+        int N = Integer.parseInt(br.readLine()); 
+
+        int[][] map = new int[N][N];
+        int[][] dp = new int[N][N];
+
+        for(int i=0; i<N; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<N; j++){
+                map[i][j] = Integer.parseInt(st.nextToken()); 
+            }
+        }
+
+        dp[0][0] = map[0][0]; 
+        for(int i=1; i<N; i++){
+            dp[i][0] = Math.min(dp[i-1][0], map[i][0]);
+            dp[0][i] = Math.min(dp[0][i-1], map[0][i]); 
+        }
+
+        for(int r=1; r<N; r++){
+            for(int c=1; c<N; c++){
+                dp[r][c] = Math.min(map[r][c], Math.max(dp[r-1][c], dp[r][c-1])); 
+            }
+        }
+
+        // for(int i=0; i<N; i++){
+        //     System.out.println(Arrays.toString(dp[i]));
+        // }
+
+        System.out.println(dp[N-1][N-1]); 
+
+    }
+}
