96f62514775fcdfd229aed5871e7573c84a1d5dd
권현준 <119548924+Kwonhyunjun@users.noreply.github.com>
2024-08-14 11:54:20 +0900

    Title: 최소 차 분할, Time: 259ms, Memory: 12MB, Status: Passed - CodeTree

--- 240814/최소 차 분할/minimum-diff-partition.java
+++ 240814/최소 차 분할/minimum-diff-partition.java
@@ -18,13 +18,29 @@ public class Main {
         boolean[] dp = new boolean[total+1];
         dp[0] = true;
 
-        for(int i=0; i<=total; i++){
-            if(!dp[i]) continue;
-            for(int j=0; j<N; j++){
-                if(i + arr[j] >= total+1) continue;
+        // for(int i=0; i<=total; i++){
+        //     if(!dp[i]) continue;
+        //     for(int j=0; j<N; j++){
+        //         if(i + arr[j] >= total+1) continue;
                 
-                dp[i + arr[j]] = true;
+        //         dp[i + arr[j]] = true;
+        //     }
+        // }
+
+        ArrayList<Integer> list; 
+        for(int i=0; i<N; i++){
+            int num = arr[i]; 
+            list = new ArrayList<>(); 
+            for(int j=num; j<=total; j++){
+                if(dp[j-num] && !dp[j]){
+                    list.add(j); 
+                }
             }
+
+            for(int idx : list){
+                dp[idx] = true; 
+            }
+
         }
 
         // System.out.println(Arrays.toString(dp)); 
