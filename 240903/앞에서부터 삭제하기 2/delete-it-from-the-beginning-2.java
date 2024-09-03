ddbc85752d8fa831b00d6b5975c80130aa55d009
권현준 <119548924+Kwonhyunjun@users.noreply.github.com>
2024-09-03 11:48:22 +0900

    Title: 앞에서부터 삭제하기 2, Time: 309ms, Memory: 21MB, Status: Passed - CodeTree

--- 240903/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.java
+++ 240903/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.java
@@ -7,86 +7,28 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine()); 
 
         int N = Integer.parseInt(st.nextToken());
-        PriorityQueue<Data> pq = new PriorityQueue<>((o1, o2) -> o1.order - o2.order);
-        PriorityQueue<Data> copy;
+        int[] arr = new int[N];
+
         st = new StringTokenizer(br.readLine());
         for(int i=0; i<N; i++){
-            pq.offer(new Data(i, Integer.parseInt(st.nextToken()))); 
+            arr[i] = Integer.parseInt(st.nextToken()); 
         }
+
+        float sum = arr[N-1]; 
+        PriorityQueue<Integer> pq = new PriorityQueue<>(); 
+        pq.offer(arr[N-1]); 
 
         float answer = 0f; 
 
-        for(int i=1; i<=N-2; i++){
-            int remove = pq.poll().num;
-            // System.out.printf("%d 삭제\n", remove); 
+        for(int i=N-2; i>=0; i--){
+            int num = arr[i]; 
+            
+            sum += num;
+            pq.offer(num); 
 
-            copy = new PriorityQueue<>((o1, o2) -> o1.num - o2.num);
-            copy.addAll(pq); 
-            copy.poll(); 
-
-            float sum = 0f; 
-            for(Data data : copy){
-                sum += data.num;
-            }
-
-            // System.out.printf("%f 나누기 %d = %f\n", sum, pq.size()-1, (sum / (pq.size()-1)));
-            answer = Math.max(answer, (sum / (pq.size()-1)));
-
+            answer = Math.max(answer, ((sum - pq.peek()) / (pq.size()-1))); 
         }
 
         System.out.printf("%.2f", answer);
-
-
     }
 }
-
-class Data{
-    int order, num;
-
-    public Data(int order, int num){
-        this.order = order;
-        this.num = num; 
-    }
-}
-
-
-// public class Main {
-//     public static void main(String[] args) throws Exception{
-//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//         StringTokenizer st = new StringTokenizer(br.readLine()); 
-
-//         int N = Integer.parseInt(st.nextToken());
-//         PriorityQueue<Integer> pq = new PriorityQueue<>(); 
-
-//         st = new StringTokenizer(br.readLine());
-//         for(int i=0; i<N; i++){
-//             pq.offer(Integer.parseInt(st.nextToken())); 
-//         }
-
-//         float answer = 0f; 
-
-//         int k = 1;
-//         while(k <= N-2){
-//             System.out.println(k); 
-//             // 삭제
-//             int remove = pq.poll(); 
-//             // System.out.printf("삭제 : %d\n", remove);
-
-//             float sum = 0f; 
-//             for(int n : pq){
-//                 sum += (float) n; 
-//             }
-
-//             sum -= pq.peek(); 
-            
-//             // System.out.printf("%f 나누기 %d = %f\n", sum, pq.size()-1, (sum / (pq.size()-1)));
-//             answer = Math.max(answer , (sum / (pq.size()-1)));
-
-//             k++; 
-//         }
-
-//         System.out.printf("%.2f", answer);
-
-        
-//     }
-// }
