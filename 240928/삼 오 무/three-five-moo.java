8dfef6b933e68d26a7a64dc8c6dbf8b5cec3145d
권현준 <119548924+Kwonhyunjun@users.noreply.github.com>
2024-09-28 13:17:43 +0900

    Title: 삼 오 무, Time: 84ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240928/삼 오 무/three-five-moo.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+
+        int left = 1;
+        int right = 1_000_000_000;
+
+        int answer = 0; 
+
+        while(left <= right){
+            
+            int mid = left + (right - left) / 2; 
+            
+            int order = check(mid); 
+
+            if(order == N){
+                answer = mid; 
+                break; 
+            }
+
+            if(order > N){
+                // 해당 숫자가 순번이 더 클경우
+                right = mid -1; 
+                
+            }else{
+                left = mid + 1; 
+            }
+
+        }
+
+        System.out.println(answer); 
+    }
+
+    static int check(int num){
+        int res = 0; 
+
+        int three = (num / 3); 
+        int five = num / 5; 
+        int fifteen = num / 15; 
+
+        res = num - (three + five - fifteen); 
+
+        return res; 
+    }
+}
