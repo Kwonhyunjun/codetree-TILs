2c7eaa25bcfba1362da35cbe1a902a25bb65339a
권현준 <119548924+Kwonhyunjun@users.noreply.github.com>
2024-10-02 16:30:08 +0900

    Title: 빙산의 일각, Time: 3090ms, Memory: 17MB, Status: Time Limit Exceed - CodeTree

--- 241002/빙산의 일각/the-tip-of-the-iceberg.java
+++ 241002/빙산의 일각/the-tip-of-the-iceberg.java
@@ -1,51 +1,84 @@
-import java.io.*;
-import java.util.*; 
+import java.io.*; 
+import java.util.*;
 
 public class Main {
+    static int N;
+    static List<Integer> heights, drown;
+    static PriorityQueue<Data> icebergs; 
 
-    static int N, arr[], low, high; 
-    static List<Integer> list; 
+    static class Data{
+        int num, height;
 
+        public Data(int num, int height){
+            this.num = num;
+            this.height = height; 
+        }
+    }
+    
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        N = Integer.parseInt(br.readLine());
-        arr = new int[N]; 
-        int answer = 0; 
 
-        low = Integer.MAX_VALUE;
-        high = Integer.MIN_VALUE;
+        N = Integer.parseInt(br.readLine());
+        heights = new ArrayList<>(); 
+        drown = new ArrayList<>(); 
+        icebergs = new PriorityQueue<>(new Comparator<Data>(){
+            
+            @Override
+            public int compare(Data o1, Data o2){
+                if(o1.height == o2.height){
+                    return o1.num - o2.num;
+                }
+                return o1.height - o2.height; 
+            }
+        });
 
         for(int i=0; i<N; i++){
-            arr[i] = Integer.parseInt(br.readLine());
-            low = Math.min(low, arr[i]); 
-            high = Math.max(high, arr[i]); 
+            int height = Integer.parseInt(br.readLine()); 
+
+            icebergs.offer(new Data(i, height)); 
+
+            if(!heights.contains(height)){
+                heights.add(height); 
+            }
         }
 
-        for(int i=low; i<high; i++){
-            list = new ArrayList<>(); 
-            for(int j=0; j<N; j++){
-                if(arr[j] - i <= 0 ){
-                    if(j==0 || j == N-1) continue; 
-                    list.add(j); 
-                }
+        Collections.sort(heights);
+        int max = Integer.MIN_VALUE; 
+
+        for(int i : heights){
+            while (icebergs.peek() != null && icebergs.peek().height == i) {
+                Data d = icebergs.poll();
+                drown.add(d.num); 
             }
 
-            if(list.isEmpty()) continue;
-
-            int cnt = 2; 
-            int prev = list.get(0); 
-            for(int j=1; j<list.size(); j++){
-                if(Math.abs(prev-list.get(j)) > 1){
-                    cnt++; 
-                }
-                prev = list.get(j); 
-            }
-            // System.out.printf("해수면(%d) -> %d\n", i, cnt); 
-            answer = Math.max(answer, cnt); 
+            int cnt = check();
+            // System.out.printf("%d => %d\n",i, cnt);
+            max = Math.max(max, cnt); 
         }
 
-        System.out.println(answer); 
+        System.out.println(max); 
+    }
+
+    static public int check(){
+        int cnt = 0; 
         
+        // 위치 조정
+        Collections.sort(drown); 
+        // System.out.println(drown);
+
+        int prev = drown.get(0); 
+
+        for(int i=1; i<drown.size(); i++){
+            if(Math.abs(prev-drown.get(i)) > 1){
+                // System.out.println(prev + " " + drown.get(i));
+                cnt++; 
+            }
+            prev = drown.get(i);
+        }
+
+        if(drown.get(0) !=0) cnt++;
+        if(drown.get(drown.size()-1) != N-1) cnt++; 
+
+        return cnt; 
     }
 }
