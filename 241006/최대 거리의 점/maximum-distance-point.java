c12fb34e57ef8ef1f83ecbdf51c0bbc9772e414f
권현준 <119548924+Kwonhyunjun@users.noreply.github.com>
2024-10-06 13:40:18 +0900

    Title: 최대 거리의 점, Time: 228ms, Memory: 13MB, Status: Wrong Answer - CodeTree

---
+++ 241006/최대 거리의 점/maximum-distance-point.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*; 
+
+public class Main {
+
+    static int N, M, points[]; 
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken()); // 점의 개수
+        M = Integer.parseInt(st.nextToken()); // 물건의 개수
+
+        points = new int[N]; 
+
+        for(int i=0; i<N; i++){
+            points[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(points); 
+
+        int left = points[0]; 
+        int right = points[N-1]; 
+        int answer = Integer.MIN_VALUE;
+
+        while(left <= right){
+            int mid = left + (right - left) / 2; 
+            // System.out.printf("%d~%d => %d\n", left, right, mid);
+
+            if(check(mid)){
+                left = mid + 1; 
+                answer = Math.max(answer, mid); 
+            }else{
+                right = mid - 1; 
+            }
+
+        }
+
+        System.out.println(answer);
+    }
+
+
+    static boolean check(int mid){
+        int cnt = 1;
+        int start = points[0];
+
+        for(int i=1; i<N; i++){
+            int dist = Math.abs(start - points[i]);
+
+            if(dist >= mid){
+                cnt++; 
+                start = points[i]; 
+            }
+        }
+
+        return cnt >= M; 
+    }
+
+
+
+   
+}
