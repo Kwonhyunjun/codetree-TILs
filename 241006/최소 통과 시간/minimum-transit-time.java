94d4252e4970d6e243229705bee491785f3010c0
권현준 <119548924+Kwonhyunjun@users.noreply.github.com>
2024-10-06 19:31:08 +0900

    Title: 최소 통과 시간, Time: 205ms, Memory: 13MB, Status: Wrong Answer - CodeTree

---
+++ 241006/최소 통과 시간/minimum-transit-time.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*; 
+
+public class Main {
+
+    static int N, M, times[], answer; 
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        answer = Integer.MAX_VALUE;
+
+        times = new int[N]; 
+        for(int i=0; i<N; i++){
+            times[i] = Integer.parseInt(br.readLine()); 
+        }
+
+        Arrays.sort(times); 
+        
+        int left = 1; 
+        int right = 1000000000; 
+
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+
+            // System.out.printf("%d~%d => %d\n", left, right, mid); 
+
+            if(isPossible(mid)){
+                right = mid - 1;
+                answer = Math.min(answer, mid); 
+            }else{
+                left = mid + 1; 
+            }
+
+        }
+
+        System.out.println(answer); 
+    }
+
+    static boolean isPossible(int mid){
+        int cnt = 0; 
+
+        for(int i=0; i<N; i++){
+            cnt += (mid / times[i]); 
+        }
+
+        return cnt >= M; 
+    }
+}
