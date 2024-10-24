61f405dd24c12df5f4a770a3308a37e55b5359d5
권현준 <119548924+Kwonhyunjun@users.noreply.github.com>
2024-10-24 13:57:27 +0900

    Title: 높은 숫자의 카드가 이기는 게임, Time: 189ms, Memory: 14MB, Status: Passed - CodeTree

--- 241024/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.java
+++ 241024/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.java
@@ -1,55 +1,43 @@
-import java.io.*;
-import java.util.*;
+import java.io.*; 
+import java.util.*; 
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 
-        int N = Integer.parseInt(br.readLine());
-        
-        ArrayList<Integer> list = new ArrayList<>();  
+        int N = Integer.parseInt(br.readLine()); 
+
+        boolean[] isSelected = new boolean[2*N+1]; 
+
         for(int i=0; i<N; i++){
-            list.add(Integer.parseInt(br.readLine()));
+            int idx = Integer.parseInt(br.readLine()); 
+            isSelected[idx] = true; 
         }
-
-        PriorityQueue<Integer> pq1 = new PriorityQueue<>();
-        PriorityQueue<Integer> pq2 = new PriorityQueue<>();
-
-        for(int i=1; i<=2*N; i++){
-            if(list.contains(i)){
-                pq1.offer(i); 
-            }else{
-                pq2.offer(i); 
-            }
-        }
-
 
         int answer = 0; 
+        int cnt = 0; 
+        int pointer = 1; 
 
-        while(!pq1.isEmpty() && !pq2.isEmpty()){
-            int cur = pq1.poll(); 
-            // System.out.println(cur);
+        while(pointer < 2*N+1){
+            // System.out.println(pointer); 
+            if(isSelected[pointer]){
+                cnt++;
+                pointer++; 
+            }else{
+                while(pointer < 2*N+1 && cnt > 0 && !isSelected[pointer]){
+                    // System.out.println("remove");
+                    answer++;
+                    cnt--;
+                    pointer++; 
+                }
 
-            boolean flag = false; 
-
-            while(!pq2.isEmpty()){
-                if(pq2.peek() > cur){
-                    flag = true; 
-                    break;
-                }else{
-                    pq2.poll();
+                while(pointer < 2*N+1 && !isSelected[pointer]){
+                    // System.out.println("move");
+                    pointer++; 
                 }
             }
-            
-
-            if(flag) answer++; 
-            pq2.poll();
-            // answer++; 
-            // System.out.printf("answer : %d\n", answer, pq2.poll());
         }
 
-        System.out.println(answer);
-
-
+        System.out.println(answer); 
     }
 }
