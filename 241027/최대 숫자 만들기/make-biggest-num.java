37274144929786e365cebe7a1dce079783cf895f
권현준 <119548924+Kwonhyunjun@users.noreply.github.com>
2024-10-27 14:14:33 +0900

    Title: 최대 숫자 만들기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241027/최대 숫자 만들기/make-biggest-num.java
+++ 241027/최대 숫자 만들기/make-biggest-num.java
@@ -12,19 +12,25 @@ public class Main {
             arr[i] = Integer.parseInt(br.readLine()); 
         }
 
+        Arrays.sort(arr);
+
         Arrays.sort(arr, new Comparator<Integer>(){
             @Override
             public int compare(Integer o1, Integer o2){
                 String s1 = String.valueOf(o1);
                 String s2 = String.valueOf(o2); 
-
+                // System.out.println(s1 + " " + s2);
                 int n1 = Integer.parseInt(s1+s2);
                 int n2 = Integer.parseInt(s2+s1);
 
-                if(n1 <= n2){
-                    return -(o1-o2);
-                }else{
+                // System.out.println(n1 + " " + n2);
+
+                if(n1 < n2){
+                    // System.out.println("n1이 더작음");
                     return o1-o2;
+                }else{
+                    // System.out.println("n2이 더작음");
+                    return -(o1-o2);
                 }
             }
         }); 
@@ -33,6 +39,7 @@ public class Main {
 
         for(int num : arr){
             sb.append(num);
+            // System.out.println("arr : " + num );
         }
 
         System.out.println(Long.parseLong(sb.toString()));
