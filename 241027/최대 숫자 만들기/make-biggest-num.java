efabce8d516f44b12269aee4c31a749b7167b1ee
권현준 <119548924+Kwonhyunjun@users.noreply.github.com>
2024-10-27 14:29:57 +0900

    Title: 최대 숫자 만들기, Time: 612ms, Memory: 32MB, Status: Passed - CodeTree

--- 241027/최대 숫자 만들기/make-biggest-num.java
+++ 241027/최대 숫자 만들기/make-biggest-num.java
@@ -20,18 +20,19 @@ public class Main {
                 String s1 = String.valueOf(o1);
                 String s2 = String.valueOf(o2); 
                 // System.out.println(s1 + " " + s2);
-                int n1 = Integer.parseInt(s1+s2);
-                int n2 = Integer.parseInt(s2+s1);
+                long n1 = Long.parseLong(s1+s2);
+                long n2 = Long.parseLong(s2+s1);
 
                 // System.out.println(n1 + " " + n2);
 
-                if(n1 < n2){
-                    // System.out.println("n1이 더작음");
-                    return o1-o2;
-                }else{
-                    // System.out.println("n2이 더작음");
-                    return -(o1-o2);
-                }
+                // if(n1 <= n2){
+                //     // System.out.println("n1이 더작음");
+                //     return o1-o2;
+                // }else{
+                //     // System.out.println("n2이 더작음");
+                //     return -(o1-o2);
+                // }
+                return Long.compare(n2, n1);
             }
         }); 
 
@@ -42,6 +43,6 @@ public class Main {
             // System.out.println("arr : " + num );
         }
 
-        System.out.println(Long.parseLong(sb.toString()));
+        System.out.println(sb);
     }
 }
